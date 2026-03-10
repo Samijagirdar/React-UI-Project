60b195a0afd9496e4d43c2eb44526e722ac7352b
Samijagirdar <mdsamijagirdar@gmail.com>
2026-03-10 13:08:18 +0530

    Added Navbar and Main content of Page 1

---
+++ src/components/Section1/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-8 px-16">
+      <h4 className="bg-black text-white rounded-full px-5 py-2 uppercase tracking-widest">
+        Target Audience
+      </h4>
+      <button className="bg-gray-200 px-5 py-2 rounded-full uppercase text-sm tracking-widest">
+        Digital Banking Platform
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Section1 = () => {
-  return <div className="h-screen w-full bg-blue-950">Section1</div>;
+  return (
+    <div className="h-screen w-full">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Section1;
