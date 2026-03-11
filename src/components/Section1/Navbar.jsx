7c80df1c3157c20b345845668124d91f6e219d87
Samijagirdar <mdsamijagirdar@gmail.com>
2026-03-11 23:31:48 +0530

    Added Navbar and Main content of Page 1

--- src/components/Section1/Navbar.jsx
+++ src/components/Section1/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-8 px-16">
-      <h4 className="bg-black text-white rounded-full px-5 py-2 uppercase tracking-widest">
+      <h4 className="bg-black text-white rounded-full px-4 py-2 uppercase tracking-widest">
         Target Audience
       </h4>
       <button className="bg-gray-200 px-5 py-2 rounded-full uppercase text-sm tracking-widest">
