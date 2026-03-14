6b0bae4d308c064ccd8060076379cbc7d4615388
Samijagirdar <mdsamijagirdar@gmail.com>
2026-03-14 13:06:35 +0530

    refactor(section1): extract HeroText component

---
+++ src/components/Section1/HeroText.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HeroText = () => {
+  return (
+    <div className="p-6">
+      <h3 className=" mb-7 text-5xl font-bold">
+        Prospective <br /> <span>Customer</span> <br /> Segmentation
+      </h3>
+      <p className="text-xl font-medium text-gray-500">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate illo
+        perferendis atque itaque nesciunt dolores alias quam libero, aut
+        corporis?
+      </p>
+    </div>
+  );
+};
+
+export default HeroText;
--- src/components/Section1/LeftContent.jsx
+++ src/components/Section1/LeftContent.jsx
@@ -1,16 +1,12 @@
 import "remixicon/fonts/remixicon.css";
+import HeroText from "./HeroText";
+import Arrow from "./Arrow";
 
 const LeftContent = () => {
   return (
-    <div className="h-full w-1/4 bg-blue-300 text-5xl font-bold">
-      <div>
-        <h3>
-          Prospective <br /> <span>Customer</span> <br /> Segmentation
-        </h3>
-      </div>
-      <div className="text-8xl">
-        <i className="ri-arrow-right-up-line"></i>
-      </div>
+    <div className="h-full w-1/3 flex flex-col justify-between">
+      <HeroText />
+      <Arrow />
     </div>
   );
 };
